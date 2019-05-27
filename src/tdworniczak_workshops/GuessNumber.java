030191c87e4e78bacfb86b91d493ca91c47ab6cb
Tomasz Dworniczak <tomasz.dworniczak@hotmail.com>
2019-05-27 08:59:37 +0200

    Zgadywanka liczb

--- src/tdworniczak_workshops/GuessNumber.java
+++ src/tdworniczak_workshops/GuessNumber.java
@@ -9,28 +9,28 @@ public class GuessNumber {
 
         Random random = new Random();
 
-        int number = random.nextInt(100) + 1;        //losuje liczbę z zakresu 1 - 100
+        int number = random.nextInt(100) + 1;
 
 //        System.out.println(number);
 
         Scanner scan = new Scanner(System.in);
-        while (true) {                                     //pętla, która wywołuje "zgadywanie" od nowa, ale dalej
-            System.out.println("Zgadnij liczbę");           // tej samej liczby, którą na początku wylosowal program
-            while (!scan.hasNextInt()) {
-                scan.nextLine();
-                System.out.println("To nie jest liczba!");
-            }
-            int n = scan.nextInt();
-//        System.out.println(n);
+        while (true) {
+            System.out.println("Zgadnij liczbę");           
+            String whatever = scan.nextLine();
+            try {
 
-            if (n > number) {
-                System.out.println("Za dużo!");
-            } else if (n < number) {
-                System.out.println("Za mało!");
-            } else {
-                System.out.println("Zgadłeś");
-                break;
+                int a = Integer.parseInt(whatever);
+                if (a > number) {
+                    System.out.println("Za dużo!");
+                } else if (a < number) {
+                    System.out.println("Za mało!");
+                } else {
+                    System.out.println("Zgadłeś");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Podana wartośc nie jest liczbą!");
             }
         }
     }
 }
+
